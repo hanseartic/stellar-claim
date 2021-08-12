1eca5dcc3216927e31dc67b302b7d0b10bc96909
Paul Schütte <paul.schuette@otto.de>
2021-08-12 07:11:42 +0200

    Add balances table
    * use AssetPresenter to show asset information

--- src/Pages/AccountOverview.tsx
+++ src/Pages/AccountOverview.tsx
@@ -1,6 +1,47 @@
-import React from 'react';
-import AccountSelector from "../AccountSelector";
+import { Table } from 'antd';
+import React, {useEffect, useState} from 'react';
+import AccountSelector from '../AccountSelector';
+import useApplicationState from "../useApplicationState";
+import AssetPresenter from "../ClaimableBalancesList/AssetPresenter";
+
+const balancesTableColumns = () => [
+    {
+        dataIndex: 'asset',
+        render: (asset: string) => <AssetPresenter code={asset} />,
+    },
+    {
+        dataIndex: 'balance',
+    }
+];
 
 export default function AccountOverview() {
-    return (<><AccountSelector /></>);
+    const { accountInformation, } = useApplicationState();
+    const [accountBalances, setAccountBalances] = useState<any[]>();
+
+    useEffect(() => {
+        if (accountInformation.account) {
+            console.log(accountInformation.account.balances);
+            setAccountBalances(accountInformation.account?.balances.map(
+                (balanceLine) => ({
+                    asset: balanceLine.asset_type !== 'native'
+                        ? `${balanceLine.asset_code}:${balanceLine.asset_issuer}`
+                        : 'native:XLM',
+                    balance: balanceLine.balance,
+                    buyingLiabilities: balanceLine.buying_liabilities,
+                    sellingLiabilities: balanceLine.selling_liabilities,
+
+                })
+            ));
+        }
+    }, [accountInformation.account]);
+
+    return (<>
+        <AccountSelector />
+        <Table
+            columns={balancesTableColumns()}
+            dataSource={accountBalances}
+            key="asset"
+            pagination={{defaultPageSize: 100}}
+        />
+    </>);
 };
