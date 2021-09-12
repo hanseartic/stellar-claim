246151a2f31af90c28750e6b2e67ad4dff3a99e2
Paul Schütte <paul@hanseartic.de>
2021-09-12 22:57:47 +0200

    Add sending assets as claimable balance.

--- src/Components/BalanceCard.tsx
+++ src/Components/BalanceCard.tsx
@@ -1,10 +1,30 @@
 import {BigNumber} from "bignumber.js";
-import {Badge, Card} from "antd";
+import {Badge, Button, Card, Input, Popover} from "antd";
+import {SendOutlined} from "@ant-design/icons";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {
+    faBalanceScaleLeft,
+    faCoins,
+    faPeopleArrows, faSatelliteDish
+} from '@fortawesome/free-solid-svg-icons';
 import React, {useEffect, useState} from "react";
-import {AccountResponse, ServerApi} from "stellar-sdk";
+import {
+    AccountResponse,
+    BASE_FEE,
+    Claimant,
+    Keypair,
+    Memo,
+    Networks,
+    Operation,
+    Server,
+    ServerApi,
+    TransactionBuilder
+} from "stellar-sdk";
 import {OfferCallBuilder} from "stellar-sdk/lib/offer_call_builder";
 import URI from "urijs";
 import StellarHelpers, {getStellarAsset} from "../StellarHelpers";
+import useApplicationState from "../useApplicationState";
+import {submitTransaction} from "./WalletHandling";
 
 export type AccountBalanceRecord = {
     account: AccountResponse,
@@ -16,8 +36,17 @@ export type AccountBalanceRecord = {
 };
 
 export default function BalanceCard({balanceRecord}: {balanceRecord: AccountBalanceRecord}) {
-    const {horizonUrl} = StellarHelpers();
+    const {accountInformation, setAccountInformation} = useApplicationState();
+    const {getSelectedNetwork, horizonUrl} = StellarHelpers();
     const [assetDemand, setAssetDemand] = useState(new BigNumber(0));
+    const [sendPopoverVisible, setSendPopoverVisible] = useState(false);
+    const [sendAmount, setSendAmount] = useState('')
+    const [sendAmountInvalid, setSendAmountInvalid] = useState(false)
+    const [destinationAccount, setDestinationAccount] = useState<AccountResponse>()
+    const [destinationAccountId, setDestinationAccountId] = useState('')
+    const [destinationAccountInvalid, setDestinationAccountInvalid] = useState(false)
+    const [xdr, setXDR] = useState('');
+    const [submitting, setSubmitting] = useState(false);
 
     const collect = (offersCollection: ServerApi.CollectionPage<ServerApi.OfferRecord>): Promise<BigNumber> => {
         if (!offersCollection.records.length) return new Promise((resolve) => resolve(new BigNumber(0)));
@@ -36,6 +65,116 @@ export default function BalanceCard({balanceRecord}: {balanceRecord: AccountBala
             );
     };
 
+    const saveXDR = () => {
+        // (sender: string, destination: string, asset: string, amount: string, network: "PUBLIC"|"TESTNET") => {
+        const claimants = [
+            new Claimant(
+                destinationAccount!.accountId(),
+                Claimant.predicateUnconditional()
+            )
+        ];
+        if (destinationAccount?.id !== accountInformation.account!.id) {
+            const claimBack = new Claimant(
+                accountInformation.account!.id,
+                Claimant.predicateNot(Claimant.predicateBeforeRelativeTime(new BigNumber(1)
+                    // .times(new BigNumber(60)) // a minute
+                    // .times(new BigNumber(60)) // an hour
+                    // .times(new BigNumber(24)) // a day
+                    // .times(new BigNumber(7))  // a week
+                    .toString())));
+            claimants.push(claimBack);
+        }
+        const transactionXDR = new TransactionBuilder(
+            accountInformation.account!,
+            {fee: BASE_FEE, networkPassphrase: Networks[getSelectedNetwork()]})
+            .addMemo(Memo.text('via balances.lumens.space'))
+            .addOperation(Operation.createClaimableBalance({
+                amount: sendAmount,
+                asset: getStellarAsset(balanceRecord.asset),
+                claimants: claimants,
+            }))
+            .setTimeout(0)
+            .build().toXDR();
+        setXDR(transactionXDR);
+    };
+
+    const sendPopoverContent = <>
+        <Input
+            allowClear
+            onChange={e => setSendAmount(e.target.value)}
+            placeholder={balanceRecord.spendable.toFormat().replace(',','')}
+            prefix={<FontAwesomeIcon icon={faCoins} />}
+            suffix={<FontAwesomeIcon icon={faBalanceScaleLeft} onClick={() =>
+                setSendAmount(balanceRecord.spendable.toFormat().replace(',',''))
+            } />}
+            value={sendAmount}
+            style={{borderColor:sendAmountInvalid?'red':undefined}}
+        />
+        <Input
+            allowClear
+            onChange={e => setDestinationAccountId(e.target.value)}
+            placeholder={Keypair.random().publicKey()}
+            prefix={<FontAwesomeIcon icon={faPeopleArrows}/>}
+            value={destinationAccountId}
+            style={{borderColor:destinationAccountInvalid?'red':undefined}}
+        />
+        <Button
+            icon={<FontAwesomeIcon icon={faSatelliteDish} />}
+            disabled={destinationAccountInvalid||destinationAccountId.length===0||sendAmountInvalid||!sendAmount}
+            loading={submitting}
+            onClick={() => saveXDR()}
+        >Transfer funds</Button>
+    </>;
+
+
+    const handleSendPopoverVisibleChange = (visible: any) => {
+        if (balanceRecord.spendable.isZero()) return;
+        setSendPopoverVisible(visible);
+    }
+
+    useEffect(() => {
+        if (xdr) {
+            setSubmitting(true);
+            submitTransaction(xdr, accountInformation.account!, horizonUrl().href, getSelectedNetwork())
+                .then(() => {
+                    console.log('sent');
+                    return new Server(horizonUrl().href)
+                        .loadAccount(accountInformation.account!.id)
+                        .then(account => setAccountInformation({account: account}));
+                })
+                .then(() => setXDR(''))
+                .then(() => {
+                    setDestinationAccount(undefined);
+                    setDestinationAccountId('');
+                    setSendAmount('');
+                    setSubmitting(false);
+                    setSendPopoverVisible(false);
+                });
+        }
+        // eslint-disable-next-line
+    }, [xdr]);
+    useEffect(() => {
+        if (destinationAccountId.length > 0) {
+            new Server(horizonUrl().href).loadAccount(destinationAccountId)
+                .then(setDestinationAccount)
+                .then(() => setDestinationAccountInvalid(false))
+                .catch(() => {
+                    setDestinationAccount(undefined);
+                    setDestinationAccountInvalid(true);
+                });
+        } else {
+            setDestinationAccountInvalid(false);
+        }
+    }, [destinationAccountId, horizonUrl]);
+    useEffect(() => {
+        setSendAmountInvalid(false);
+        if (sendAmount !== undefined && sendAmount !== '') {
+            const send = new BigNumber(sendAmount.replace(',',''));
+            if (send.greaterThan(balanceRecord.spendable) || send.isZero() || send.lessThan('0.0000001')) {
+                setSendAmountInvalid(true);
+            }
+        }
+    }, [balanceRecord.spendable, sendAmount]);
     useEffect(() => {
         if (balanceRecord.asset !== 'native:XLM') {
             new OfferCallBuilder(new URI(horizonUrl()))
@@ -72,5 +211,20 @@ export default function BalanceCard({balanceRecord}: {balanceRecord: AccountBala
                     <p>{balanceRecord.balance.sub(balanceRecord.spendable).toFormat()} reserved</p>
                     <b>{balanceRecord.balance.toFormat()} total</b>
                 </Card>
-            </Badge.Ribbon></Badge.Ribbon></Badge.Ribbon></>);
+            </Badge.Ribbon></Badge.Ribbon></Badge.Ribbon>
+        <Popover
+            trigger="click"
+            placement="leftBottom"
+            visible={sendPopoverVisible}
+            onVisibleChange={handleSendPopoverVisibleChange}
+            content={sendPopoverContent}
+            >
+            <Button
+                block
+                disabled={balanceRecord.spendable.isZero()}
+                icon={<SendOutlined />}
+                >Send
+            </Button>
+        </Popover>
+            </>);
 }
