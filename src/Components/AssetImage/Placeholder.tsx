f28ea4436428233a401698040706e493b29e4ef3
Paul Schütte <paul@hanseartic.de>
2022-05-10 14:02:58 +0200

    Use litemint preview images

---
+++ src/Components/AssetImage/Placeholder.tsx
@@ -0,0 +1,58 @@
+import {Image as AntImage, ImageProps, Skeleton} from "antd";
+import {Asset} from "stellar-sdk";
+import React, {useEffect, useMemo, useState} from "react";
+
+type PlaceholderProps = {
+    asset: Asset;
+    domain: string;
+    originalSource?: string;
+    onPreview?: (url: string) => void;
+};
+
+const Placeholder = (props: ImageProps & PlaceholderProps) => {
+    const {asset, onPreview, originalSource} = props;
+    const skeleton = useMemo(() => <Skeleton avatar={{shape:"square"}} active={true} />, []);
+    const [extension, setExtension] = useState<string>();
+    const url = useMemo(
+        () => `https://litemint.azureedge.net/userdata/original-${asset.code}-${asset.issuer}-256.${extension}`,
+        [extension, asset]
+    );
+    useEffect(() => {
+        if (originalSource?.includes("https://ipfs.io")) {
+            fetch(originalSource, {method:"GET", headers: {"Range": "bytes=0-0"}})
+                .then(res => res.blob())
+                .then(blob => {
+                    if (blob.type.includes("image/")) {
+                        setExtension(blob.type.split("/")[1]);
+                    }
+                })
+                .catch(console.debug);
+        } else if (originalSource) {
+            onPreview?.(originalSource);
+        }
+    }, [originalSource, onPreview]);
+
+    const [loadingFailed, setLoadingFailed] = useState(false);
+
+    const handleLoadError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+        e.preventDefault();
+        e.stopPropagation();
+        console.debug("failed to load preview from url", url);
+        setLoadingFailed(true);
+        if (originalSource) onPreview?.(originalSource);
+    };
+
+    const imageProps = {...props, onPreview: undefined, originalSource: undefined, src: undefined, asset: undefined, domain: undefined};
+    return <>{props.domain.includes("litemint.store") && !loadingFailed && extension
+        ? <AntImage
+            {...imageProps}
+            src={url}
+            onError={handleLoadError}
+            onLoad={() => onPreview?.(url)}
+            preview={false}
+            placeholder={skeleton}
+        />
+        : skeleton}</>;
+}
+
+export default Placeholder;
--- src/Components/AssetImage/index.tsx
+++ src/Components/AssetImage/index.tsx
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import {TomlAssetInformation, useAssetIsStroopsAsset} from '../../StellarHelpers';
-import {Avatar, Image, Skeleton} from 'antd';
+import {Avatar, Image as AntImage} from 'antd';
 import stellarLogo from '../../stellar_logo_black.png';
 import { Asset } from 'stellar-sdk';
 import './style.css';
+import Placeholder from "./Placeholder";
 
 interface AssetImageProps {
     asset: Asset,
@@ -12,6 +13,7 @@ interface AssetImageProps {
 
 interface loadingState {
     source?: string;
+    preview?: string;
     finished: boolean;
     error: boolean;
 }
@@ -22,21 +24,20 @@ export default function AssetImage ({asset, assetInformation}: AssetImageProps) 
 
     useEffect(() => {
         if (asset.isNative()) {
-            setState(s => ({...s, source: stellarLogo}));
+            setState(s => ({...s, preview: stellarLogo}));
         } else if (assetInformation.code?.startsWith('0x')) {
             setState(s => ({...s,
-                source: 'https://twemoji.maxcdn.com/v/latest/72x72/'+assetInformation.code?.substring(2).replace(/^0+/g, '').toLowerCase()+'.png',
+                preview: 'https://twemoji.maxcdn.com/v/latest/72x72/'+assetInformation.code?.substring(2).replace(/^0+/g, '').toLowerCase()+'.png',
             }));
         } else {
             setState(s => ({...s,
-                source: s.error?s.source:assetInformation.image,
+                source: s.error?s.source:assetInformation.image, preview: undefined,
             }));
         }
-    }, [asset, assetInformation.code, assetInformation.image, state.error]);
+    }, [asset, assetInformation.code, assetInformation.image, assetInformation.domain, state.error]);
 
     const shape = nft?'square':'circle';
     const className = nft?'hexagon':'';
-    const loader = <Skeleton avatar={{shape:shape}} active />;
 
     const onImageLoadingError = () => {
         setState({finished: true, error: true, source: undefined});
@@ -50,13 +51,24 @@ export default function AssetImage ({asset, assetInformation}: AssetImageProps) 
         shape={shape}
         size={40}
         className={className}
-        src={!state.error && <Image
+        src={!state.error && <AntImage
             onLoad={onImageLoaded}
             onError={onImageLoadingError}
             wrapperClassName={"avatar-image"}
-            src={state.source}
-            preview={{mask: <></>}}
-            placeholder={state.finished?null:loader} />}
+            src={state.preview}
+            preview={{
+                mask: <></>,
+                src: state.source
+            }}
+            placeholder={<Placeholder
+                className={className}
+                width={40} height={40}
+                asset={asset}
+                domain={assetInformation.domain??""}
+                originalSource={state.source}
+                onPreview={url => setState(p => ({...p, preview: url}))}
+            />}
+        />}
         children={(
             (assetInformation.code??'')
                 // get only uppercase characters - if any
