4528b974572becafcaea6666d20a68fcc1092f23
Paul Schütte <paul@hanseartic.de>
2022-05-16 23:19:03 +0200

    Allow serving from node

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 
-const port = 32879
+const port = process.env.APP_PORT??3000
 require('./app').listen(port, () => {
     console.log(`Claim APP listening on port ${port}.`)
 })
+
+process.on('SIGINT', function() {
+    console.log("stopping");
+    process.exit(0);
+});
