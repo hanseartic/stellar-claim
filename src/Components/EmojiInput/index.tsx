338e5850aba68a328184e67d7edf93e70d2ff522
Paul <paul@hanseartic.de>
2023-02-09 11:36:00 +0100

    Updates emoji-mart (#53)

--- src/Components/EmojiInput/index.tsx
+++ src/Components/EmojiInput/index.tsx
@@ -1,6 +1,8 @@
 import {AutoComplete, Input} from "antd";
 import {InputProps} from "antd/lib/input/Input";
-import {BaseEmoji, EmojiData, EmojiEntry, emojiIndex} from "emoji-mart";
+import data, { Emoji } from '@emoji-mart/data'
+import { init, SearchIndex } from 'emoji-mart'
+
 import { DefaultOptionType } from "antd/lib/select";
 import {useState} from "react";
 
@@ -17,6 +19,8 @@ export interface EmojiInputProps extends Omit<InputProps, 'onChange'|'value'> {
 
 export const emojiShortcodeMatch = /:[_+\-a-z0-9]*:?/;
 export default function EmojiInput(props: EmojiInputProps) {
+    init({data});
+
     const [autocompleteOptions, setAutocompleteOptions] = useState<OptionsType>([]);
 
     const handleChange = (search?: string) => {
@@ -27,43 +31,19 @@ export default function EmojiInput(props: EmojiInputProps) {
         handleChange(changedData);
     };
 
-    const isEmojiData = (emoji: EmojiEntry): boolean => {
-        return Object.getOwnPropertyNames(emoji).includes('id');
-    };
-
     const onEmojiSearch = (searchText: string): void => {
         const match = searchText.match(emojiShortcodeMatch);
         if (match?.length) {
             const colonSearch = match[0].replaceAll(':', '');
 
-            const suggestions = Object.values(emojiIndex.emojis)
-                .flatMap((emoji: EmojiEntry) => (isEmojiData(emoji)?emoji:Object.values(emoji)) as EmojiData[])
-                .filter(e => (e.name.toLowerCase().search(colonSearch) >= 0)
-                    || (e.id?.search(colonSearch)??-1) >= 0
-                    || (e.colons?.search(colonSearch)??-1) >= 0)
-                .map<AutoCompleteOptionType>((emojiData) => ({
-                    label: (emojiData.name??''),
-                    options: [{value: (emojiData as BaseEmoji).native}],
-                }));
-
-            const groupedSuggestions = suggestions
-                // get unique labels
-                .map<string>(s => s.label)
-                .filter((label, i, labels) => labels.indexOf(label) === i)
-                // map merge all options with a given label in a single object
-                .map<AutoCompleteOptionType>(label => {
-                    const options = suggestions
-                        .filter(suggestion => suggestion.label === label)
-                        .flatMap<OptionsEntry>(s => s.options)
-                        .filter((currentOption, index, allOptions) => allOptions
-                            .map(o => o.value).indexOf(currentOption.value) === index
-                    );
-
-                    return ({
-                        label: label,
-                        options: options,
-                })});
-            setAutocompleteOptions(groupedSuggestions);
+            SearchIndex.search(colonSearch)
+                .then((results: Emoji[]) => results
+                    .map<AutoCompleteOptionType>(emoji => ({
+                        label: emoji.name,
+                        options: emoji.skins.map(skin => ({value: skin.native})),
+                    }))
+                )
+                .then(setAutocompleteOptions);
         } else {
             setAutocompleteOptions([]);
         }
